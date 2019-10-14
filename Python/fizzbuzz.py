cca2136c75a5ef3faa0856e51370284bae05049a
Fred-Shurte <56058234+Fred-Shurte@users.noreply.github.com>
2019-10-14 15:42:59 +0900

    My FizzBuzz solution

--- Python/fizzbuzz.py
+++ Python/fizzbuzz.py
@@ -3,12 +3,13 @@ Prints integers from 1 to 100. Multiples of three print "Fizz",
 multiples of five print "Buzz". Multiples of both three and five print "FizzBuzz"
 """
 
-for i in range (1, 101):
-    if (i % 3 == 0) and (i % 5 == 0):
-        print("FizzBuzz")
-    elif i % 3 == 0:
-        print("Fizz")
-    elif i % 5 == 0:
-        print("Buzz")
-    else:
-        print(i)
+def fizzbuzz():
+    for i in range(1,101):
+        if (i % 3 ==0) and (i % 5 == 0):
+            print(f"Index({i}): FizzBuzz")
+        elif i % 3 == 0:
+            print(f"Index({i}): Fizz")
+        elif i % 5 == 0:
+            print(f"Index({i}): Buzz")
+        else:
+            print(f"Index({i}): {i}")
