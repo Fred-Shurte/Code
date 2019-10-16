97ba8d086b68154fea08e77e2f6e8fc9881860b3
Fred-Shurte <56058234+Fred-Shurte@users.noreply.github.com>
2019-10-16 19:17:30 +0900

    My fizzbuzz solution

--- Python/fizzbuzz.py
+++ Python/fizzbuzz.py
@@ -5,7 +5,7 @@ multiples of five print "Buzz". Multiples of both three and five print "FizzBuzz
 
 def fizzbuzz():
     for i in range(1,101):
-        if (i % 3 ==0) and (i % 5 == 0):
+        if (i % 3 == 0) and (i % 5 == 0):
             print(f"Index({i}): FizzBuzz")
         elif i % 3 == 0:
             print(f"Index({i}): Fizz")
